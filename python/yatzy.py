a0e8fa1e7ce8e28f2ba52b017038ac8bc26f6c40
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-01-05 16:58:06 +0100

    refactor: small_straight and full_house

--- python/yatzy.py
+++ python/yatzy.py
@@ -101,20 +101,17 @@ class Yatzy:
         return score
 
     @staticmethod
-    def small_straight(d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-                tallies[4] == 1):
-            return 15
-        return 0
+    def small_straight(*dices):
+        orderedList = sorted(list(dices))
+        score = 0
+        smallest = 1
+        for dice in orderedList:  # while
+            if dice == smallest and orderedList.count(dice) == 1:
+                score += dice
+                smallest += 1
+            else:
+                return 0
+        return score
 
     @staticmethod
     def large_straight(*dices):
@@ -130,36 +127,8 @@ class Yatzy:
         return score
 
     @staticmethod
-    def full_house(d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i+1
-
-        for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i+1
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
-
-
-if __name__ == "__main__":
-    assert 0 == Yatzy.large_straight(1, 2, 2, 4, 5)
+    def full_house(*dices):
+        score = 0
+        if Yatzy.pair(*dices) and Yatzy.three_of_a_kind(*dices):
+            score = Yatzy.pair(*dices) + Yatzy.three_of_a_kind(*dices)
+        return score
